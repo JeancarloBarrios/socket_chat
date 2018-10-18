d397d42ab9fdbfcf2c0c58d667a9c700cc48a9ff
Jeancarlo Barrios <jeancarlobarrios@gmail.com>
2018-10-18 10:50:54 -0600

    TEST

--- client.c
+++ client.c
@@ -1,4 +1,79 @@
-//
-// Created by ebon1 on 10/15/18.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+
+    if (argc < 3) {
+        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        exit(0);
+    }
+
+    portno = atoi(argv[2]);
+
+    /* Create a socket point */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    server = gethostbyname(argv[1]);
+
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    /* Now connect to the server */
+    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR connecting");
+        exit(1);
+    }
+
+    /* Now ask for a message from the user, this message
+       * will be read by server
+    */
+
+    while (1) {
+        bzero(buffer, 255);
+        fgets(buffer, 255, stdin);
+        n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            exit(1);
+        }
+
+        bzero(buffer, 255);
+        n = read(sockfd, buffer, 255);
+
+        if (n < 0) {
+            perror("ERROR reading from socket");
+            exit(1);
+        }
+
+        printf("%s\n",buffer);
+        int i = strncmp("bye", buffer, 3);
+        if(i==0)
+            break
+
+    }
+    close(sockfd);
+    return 0;
+}
--- server.c
+++ server.c
@@ -16,6 +16,9 @@
 #include <sys/queue.h>
 #include <string.h>
 
+#include <time.h>
+
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
 
@@ -25,10 +28,16 @@
 #define POLL_SIZE 32
 #define LISTEN_QUEUE 5
 
+void waitFor (unsigned int secs) {
+    unsigned int retTime = time(0) + secs;   // Get finishing time.
+    while (time(0) < retTime);               // Loop until it arrives.
+}
+
 struct user {
     int id;
     char* name;
     char* status;
+    LIST_ENTRY(user) pointers;
 };
 
 struct message {
@@ -70,13 +79,25 @@ struct json_object * error_json(struct response_error error){
 struct json_object * user_json_obj(struct user user){
     struct json_object *tmp, *user_obj;
     user_obj = json_object_new_object();
-    tmp = json_object_new_int(user.id);
+    char id[20];
+    sprintf(id, "%d", user.id);
+    tmp = json_object_new_string(id);
     json_object_object_add(user_obj, "id", tmp);
     tmp = json_object_new_string(user.name);
     json_object_object_add(user_obj, "name", tmp);
     tmp = json_object_new_string(user.status);
     json_object_object_add(user_obj, "status", tmp);
     return user_obj;
+}
+
+
+struct json_object * list_user_base(struct json_object *users){
+    struct json_object *tmp, *object;
+    object = json_object_new_object();
+    tmp = json_object_new_string("LIST_USER");
+    json_object_object_add(object, "action", tmp);
+    json_object_object_add(object, "users", users);
+    return object;
 }
 
 struct json_object * new_user_action(struct user user, char *action){
@@ -97,18 +118,28 @@ struct json_object * accept_user(struct user user){
     return object;
 }
 
-struct user * reducer(struct user state[], json_object * obj){
+struct json_object * status_ok(){
     struct json_object *tmp, *object;
-    if (json_object_object_get_ex(obj, "action", &tmp) == 1){
-
-    } else if (json_object_object_get_ex(obj, "user", &tmp) == 1 && json_object_object_get_ex(obj, "origin", &tmp) == 1 ){
-
-    }
+    object = json_object_new_object();
+    tmp = json_object_new_string("OK");
+    json_object_object_add(object, "status", tmp);
+    return object;
 }
+
+//struct user * reducer(struct user state[], json_object * obj){
+//    struct json_object *tmp, *object;
+//    if (json_object_object_get_ex(obj, "action", &tmp) == 1){
+//
+//    } else if (json_object_object_get_ex(obj, "user", &tmp) == 1 && json_object_object_get_ex(obj, "origin", &tmp) == 1 ){
+//
+//    }
+//}
+
+
 
 int main(int argc, char *argv[]) {
 //    JSON TEST
-    struct json_object *jobj;
+    struct json_object *jobj, *tmp, *tmp2;
 //    char *str = "{\"test\": \"test\"}";
     struct user user;
     user.status = "active";
@@ -120,15 +151,42 @@ int main(int argc, char *argv[]) {
     jobj = json_tokener_parse(str);
     struct json_object* test;
 //    json_object_object_get_ex(jobj, "test", &test);
-    if (json_object_object_get_ex(jobj, "test2", &test) != NULL){
+    if (json_object_object_get_ex(jobj, "test2", &test) != 0){
 
         printf("%s", json_object_to_json_string(test));
     }
     else {
-        printf("Key Does not exisst");
+        printf("Key Does not exisst\n");
     }
 
 //    LIST TEST
+//    LIST_HEAD(user_list, user) head;
+//    struct listhead *headp;
+//    LIST_INIT(&head);
+//    struct user *n1, *n2, *np;
+////    LIST_ENTRY(user) name;
+//    n1 = malloc(sizeof(struct user));
+//    n1->id = 10;
+//    LIST_INSERT_HEAD(&head, n1, pointers);
+//    n2 = malloc(sizeof(struct user));      /* Insert after. */
+//    n2->id = 20;
+//    LIST_INSERT_AFTER(n1, n2, pointers);
+//    LIST_FOREACH(np, &head, pointers)
+//        printf("ID is: %d \n", np -> id);
+//
+////    LIST_REMOVE(n1, pointers);
+//    LIST_FOREACH(np, &head, pointers)
+//        printf("ID is: %d \n", np -> id);
+//
+//    n1 = LIST_FIRST(&head);
+//    do {
+//        if (n1 ->id == 20){
+//            break;
+//        }
+//        n2 = LIST_NEXT(n1, pointers);
+//        n1 = n2;
+//    } while (n1 != NULL);
+//    printf("THE VALUE was found : %d \n", n1->id);
 
 
 
@@ -193,6 +251,13 @@ int main(int argc, char *argv[]) {
     int compress_array = FALSE;
     int poll_flag, current_size, close_conn, rx, tx, len;
     int end_server = FALSE;
+    char * action, response;
+
+    //  List Init
+    LIST_HEAD(user_list, user) head;
+    struct listhead *headp;
+    LIST_INIT(&head);
+    struct user *node_1, *node_2, *node_tmp;
 
     while(end_server == FALSE) {
         printf("Starting POLL...\n");
@@ -258,19 +323,150 @@ int main(int argc, char *argv[]) {
                     jobj = json_tokener_parse(buffer);
                     printf("  %d bytes received\n", len);
                     printf("JSON is:  %s \n", json_object_to_json_string(jobj));
-                    if (json_object_object_get_ex(jobj, "user", &test) != NULL){
+                    //  Posible Actions to Take
 
-                        printf("Key exists congrats %s\n", json_object_to_json_string(test));
+                    if (json_object_object_get_ex(jobj, "action", &tmp) == 1){
+                        action = json_object_get_string(tmp);
+                        printf("ACTION: %s", action);
+                        if (strcmp (action, "SEND_MESSAGE") == 0)
+                        {
+                            struct message *m;
+                            m = malloc(sizeof(struct message));
+                            m->action = "RECEIVE_MESSAGE";
+                            json_object_object_get_ex(jobj, "from", &tmp);
+                            m->from = json_object_get_string(tmp);
+                            json_object_object_get_ex(jobj, "to", &tmp);
+                            m->to = json_object_get_string(tmp);
+                            json_object_object_get_ex(jobj, "message", &tmp);
+                            m->message = json_object_get_string(tmp);
+                            tmp = message_json(*m);
+                            node_1 = LIST_FIRST(&head);
+                            while (node_1 != NULL){
+                                printf("Check node %d : %d \n", node_1->id, atoi(m->to));
+                                if (node_1->id == atoi(m->to)){
+                                    char * response;
+                                    response = json_object_to_json_string(message_json(*m));
+                                    if (send(node_1->id, response, strlen(response), 0) < 0){
+                                        perror("  send() failed");
+                                        close_conn = TRUE;
+                                        break;
+                                    }
+                                    char* ok = json_object_to_json_string(status_ok());
+                                    printf("RESPOND OK: %s \n", ok);
+                                    if (send(poll_set[i].fd, ok, strlen(ok), 0) < 0){
+                                        perror("  send() failed");
+                                        close_conn = TRUE;
+                                        break;
+                                    }
+                                    break;
+                                }
+                                node_2 = LIST_NEXT(node_1, pointers);
+                                if (node_2 == NULL){
+                                    break;
+                                }
+                                node_1 = node_2;
+                            }
+                        }
+                        if (strcmp (action, "LIST_USER") == 0)
+                        {
+
+                            struct json_object *array;
+                            array = json_object_new_array();
+                            node_1 = LIST_FIRST(&head);
+                            while (node_1 != NULL)
+                            {
+                                json_object_array_add(array, user_json_obj(*node_1));
+                                node_2 = LIST_NEXT(node_1, pointers);
+                                if (node_2 == NULL){
+                                    break;
+                                }
+                                node_1 = node_2;
+                            }
+                            char * response = json_object_to_json_string(list_user_base(array));
+                            printf("LIST_USER, %s", response);
+                            if (send(poll_set[i].fd, response, strlen(response), 0) < 0){
+                                perror("  send() failed");
+                                close_conn = TRUE;
+                                break;
+                            }
+
+                        }
+
+                    } else if (json_object_object_get_ex(jobj, "origin", &tmp) == 1 && json_object_object_get_ex(jobj, "user", &tmp) == 1){
+                        printf("ADD USER TO LIST \n");
+                        node_tmp = malloc(sizeof(struct user));
+                        node_tmp->id = poll_set[i].fd;
+                        node_tmp->name = json_object_get_string(tmp);
+                        node_tmp->status = "active";
+
+                        if (LIST_EMPTY(&head) == 1) {
+                            printf("FIRST USER ADDED \n");
+                            LIST_INSERT_HEAD(&head, node_tmp, pointers);
+                            printf("LIST INSERT \n");
+                        }
+                        else {
+                            int exist = FALSE;
+                            node_1 = LIST_FIRST(&head);
+                            while (node_1 != NULL) {
+                                if (node_1 ->id == node_tmp->id){
+                                    exist = TRUE;
+                                    break;
+                                }
+                                node_2 = LIST_NEXT(node_1, pointers);
+                                if (node_2 == NULL){
+                                    break;
+                                }
+                                node_1 = node_2;
+                            }
+                            if (exist == FALSE){
+                                LIST_INSERT_BEFORE(node_1, node_tmp, pointers);
+                            }
+                        }
+//                        response = malloc(sizeof(response));
+                        char * response;
+                        response = json_object_to_json_string(accept_user(*node_tmp));
+                        printf("RESPONSE IS: %s \n", response);
+                        if (send(poll_set[i].fd, response, strlen(response), 0) < 0){
+                            perror("  send() failed");
+                            close_conn = TRUE;
+                            break;
+                        }
+
+                        node_1 = LIST_FIRST(&head);
+
+                        response = json_object_to_json_string(new_user_action(*node_tmp, "USER_CONNECTED"));
+                        while (node_1 != NULL) {
+                            if (node_1->id == poll_set[i].fd){
+                                node_2 = LIST_NEXT(node_1, pointers);
+                                if (node_2 == NULL){
+                                    break;
+                                }
+                                node_1 = node_2;
+                                continue;
+                            }
+                            printf("RESPONSE IS: %s \n", response);
+                            if (send(node_1->id, response, strlen(response), 0) < 0){
+                                perror("  send() failed");
+                                close_conn = TRUE;
+                                break;
+                            }
+                            node_2 = LIST_NEXT(node_1, pointers);
+                            if (node_2 == NULL){
+                                break;
+                            }
+                            node_1 = node_2;
+                        }
+
+
                     }
-                    else {
-                        printf("Key Does not exisst");
-                    }
+                    LIST_FOREACH(node_tmp, &head, pointers)
+                    printf("ID in Node is: %d \n", node_tmp -> id);
 //                    tx = send(poll_set[i].fd, buffer, rx, 0);
-                    if (send(poll_set[i].fd, buffer, rx, 0) < 0){
-                        perror("  send() failed");
-                        close_conn = TRUE;
-                        break;
-                    }
+//                    if (send(poll_set[i].fd, buffer, rx, 0) < 0){
+//                        perror("  send() failed");
+//                        close_conn = TRUE;
+//                        break;
+//                    }
 
                     if (TRUE){
                         break;
